0bed407dab0613af46b7193a090a9ee6ecc06ed7
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2021-01-23 13:16:45 +0330

    Setup parse server

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ThemeProvider, Text } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Parse from 'parse/react-native.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import theme from './src/theme';
 
 import { MainStackNavigator } from './src/navigation';
 
-const Stack = createStackNavigator();
+Parse.setAsyncStorage(AsyncStorage);
+
+Parse.initialize("123456");
+
+Parse.serverURL = 'http://localhost:1337/parse';
 
 export default function App() {
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
